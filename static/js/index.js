a9d123c007f00acc6ba68d475d87d2c2f69ff9ed
JareBear <jaredgrozz@gmail.com>
2024-10-02 09:48:14 -0500

    organization and updates

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     function scaleApp() {
-        let scaleRatio = Math.min(window.innerWidth / 500, window.innerHeight / 1200);
-        scaleRatio = Math.min(scaleRatio, 1);
+        let scaleRatio = 1;
+        if (window.innerWidth < 500){
+            scaleRatio = Math.min(window.innerWidth / 600, window.innerHeight / 1800);
+            scaleRatio = Math.min(scaleRatio, 1);
+        }
         document.body.style.transform = `scale(${scaleRatio})`;
         document.body.style.transformOrigin = "top left";  // Ensures scaling starts from the top-left corner
         document.body.style.width = `${100 / scaleRatio}%`;  // Adjust the width to avoid clipping
         document.body.style.height = `${100 / scaleRatio}%`; // Adjust the height similarly
     }
     scaleApp();
-
     window.addEventListener("resize", scaleApp);
 });
